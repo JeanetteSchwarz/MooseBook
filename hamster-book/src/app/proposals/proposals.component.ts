ef03076738d237d24cd5ba61355923e48a94e048
Jeanette Schwarz <jeanette.schwarz@t-online.de>
2024-04-25 22:24:59 +0200

    newmooose

--- hamster-book/src/app/proposals/proposals.component.ts
+++ hamster-book/src/app/proposals/proposals.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './proposals.component.css'
 })
 export class ProposalsComponent {
-
+names = ['Susi', 'Sven', 'Ingo', 'Monika', 'Anette']
+images = ['/assets/img/elk4.jpg', '/assets/img/elk5.jpg', '/assets/img/elk6.jpg', '/assets/img/elk3.jpg', '/assets/img/elk2.jpg']
 }
